e12076415a80b94c80e0125f03b0c7c90027a9a7
stpn48 <133258815+stpn48@users.noreply.github.com>
2024-12-06 18:53:22 +0100

    feat: improve layout add bg blur to the navbar

--- app/(app)/_components/navbar.tsx
+++ app/(app)/_components/navbar.tsx
@@ -1,3 +1,3 @@
 export function Navbar() {
-  return <div className="h-[48px] bg-background border-b border-border shadow-md"></div>;
+  return <div className="h-[48px] sticky top-0 backdrop-blur-sm bg-background/80 border-b border-border">Navbar</div>;
 }
--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -4,12 +4,12 @@ import { Navbar } from "./_components/navbar";
 
 export default function AppLayout({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="grid grid-rows-[auto,1fr] flex-1">
-        <Navbar />
-        <main className="flex-1">{children}</main>
-      </div>
-      <Footer />
+    <div className="h-screen flex-1">
+      <Navbar />
+      <main className="flex-1 overflow-auto">
+        {children}
+        <Footer />
+      </main>
     </div>
   );
 }

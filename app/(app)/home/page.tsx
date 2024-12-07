ef78d6fa729750e47b98205641cbfeab594389f4
stpn48 <133258815+stpn48@users.noreply.github.com>
2024-12-07 12:16:41 +0100

    feat: imrpove theme, add glove icon in the background of find race section

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,16 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { Earth } from "lucide-react";
 
 export default function HomePage() {
   return (
     <div className="bg-background h-full w-full min-h-[75vh] flex flex-col items-center justify-center">
       <section className="grid gap-10 w-full max-w-4xl">
         {/* Top Section */}
-        <section className="h-48 p-6 flex flex-col justify-between gap-4 border border-border rounded-lg">
+        <section className="h-48 p-6 flex flex-col relative justify-between overflow-hidden gap-4 border z-10 border-border rounded-lg">
+          <Earth strokeWidth={1.2} className="absolute size-[600px] -top-[160px] right-[20px] transform rotate-12 text-muted opacity-50 z-0" />
+
           <div className="flex flex-col">
             <h1 className="text-2xl font-bold">Find Race</h1>
             <p className="text-muted-foreground">Race against other players</p>
           </div>
-          <Button className="w-full h-10 text-base">Find Race</Button>
+          <Button className="w-full h-10 z-10 text-base">Find Race</Button>
         </section>
 
         {/* Bottom Section */}
